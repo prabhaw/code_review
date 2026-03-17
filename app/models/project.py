2c3aefeaf4339df0fc42f91c19d757990d43e4fd
prabhaw <prabhaw.soti@ebpearls.com>
2026-03-17 17:13:11 +0545

    initial work

--- app/models/project.py
+++ app/models/project.py
@@ -16,4 +16,4 @@ class User(Document):
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     class Settings:
-        name = "users"
+        name = "projects"
